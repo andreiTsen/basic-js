96dd8a340c00987a6f1943eeae119f73f9dd7486
Andrei Tseniuta <gomelmf@gmail.com>
2024-12-21 15:21:52 +0100

    third next functions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+
+  addLink(value) {
+    this.chain.push(`( ${String(value)} )`);
+    return this;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+
+  removeLink(position) {
+    if (position < 1 || position > this.chain.length || !Number.isInteger(position)) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!"); 
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
     // remove line with error and write your code here
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this;
     // remove line with error and write your code here
   },
+  
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    const result = this.chain.join('~~');
+    this.chain = [];
+    return result;
     // remove line with error and write your code here
   }
 };
